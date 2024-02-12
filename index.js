91ec79e34783561d92fcedf40f026f0bac6e23ad
CubeBeveled <cubebeveled@gmail.com>
2024-02-12 18:19:40 +0100

    Added folder support

--- index.js
+++ index.js
@@ -9,15 +9,41 @@ app.use(express.static(__dirname + "/Public")); // Serve static files from the r
 
 app.listen(port, () => { console.log("\x1b[32m", `Server is running on port ${port}`) });
 
-app.get("/", (req, res) => {
-  fs.readdir("Public/Files", (err, files) => {
-    if (err) {
-      return res.status(500).send("Error reading the directory.");
-    }
 
-    const fileList = files.map(
-      file => `<div class="file-container"><a href="Files/${file}" class="file"><img src="fileimg.png" class="file-img">${file}</a><a href="Files/${file}" class="file" download><button class="button">Download</button></a></div>`
-    ).join("");
+app.get("/", async (req, res) => {
+  const files = await getContents("Public/Files")
+    .catch((err) => {
+      return { err: err };
+    });
+
+  let finalList = [];
+
+  if (files.err) {
+    console.log(files.err)
+    res.status(500).send("Error reading the directory.");
+  } else {
+    for (const i of files) {
+      if (i.type == "file") {
+        finalList.push(
+          `<div class="file-container">
+            <a href="Files/${i.name}" class="file">
+              <img src="file.png" class="icon">
+              ${i.name}
+            </a>
+            <a href="Files/${i.name}" class="file" download>
+              <button class="button">Download</button>
+            </a>
+          </div>`
+        )
+      } else {
+        finalList.push(
+          `<div class="file-container">
+            <img src="folder.png" class="icon">
+            ${i.name}
+          </div>`
+        )
+      }
+    }
 
     const html = `
       <!DOCTYPE html>
@@ -29,11 +55,53 @@ app.get("/", (req, res) => {
         <title>Files</title>
       </head>
       <body>
-        ${fileList}
+        ${finalList.join("")}
       </body>
       </html>
     `;
 
     res.send(html);
-  });
+  }
 });
+
+function isDirectory(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(stats.isDirectory());
+      }
+    });
+  });
+}
+
+function getContents(directoryPath) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(directoryPath, { withFileTypes: true }, (err, entries) => {
+      if (err) {
+        reject(err);
+      } else {
+        let result = [];
+
+        entries.forEach(async (entry) => {
+          const entryPath = path.join(directoryPath, entry.name);
+          // If its a directory, recursively list its contents (not anymore)
+          if (entry.isDirectory()) {
+            result.push({
+              name: entry.name,
+              type: "dir",
+            });
+          } else {
+            result.push({
+              name: entry.name,
+              type: "file",
+            });
+          }
+        });
+
+        resolve(result);
+      }
+    });
+  });
+}
