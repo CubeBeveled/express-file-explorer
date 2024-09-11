520b18b77ce0afaacbf3e8ab52979b0f2d266af6
CubeBeveled <cubebeveled@gmail.com>
2024-09-11 16:27:12 +0200

    added ALOT of icons for file types

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const color = require("colors");
 const path = require("path");
 const fs = require("fs")
 const app = express();
@@ -27,7 +28,7 @@ app.get("/", async (req, res) => {
         finalList.push(
           `<div class="file-container">
             <a href="files/${i.name}" class="file">
-              <img src="assets/file.png" class="icon">
+              <img src="assets/icons/${getIcon(i.name)}" class="icon">
               ${i.name}
             </a>
             <a href="files/${i.name}" class="file" download>
@@ -35,10 +36,10 @@ app.get("/", async (req, res) => {
             </a>
           </div>`
         )
-      } else {
+      } else if (i.type == "dir") {
         finalList.push(
           `<div class="file-container">
-            <img src="assets/folder.png" class="icon">
+            <img src="assets/icons/folder.svg" class="icon">
             ${i.name}
           </div>`
         )
@@ -63,18 +64,6 @@ app.get("/", async (req, res) => {
     res.send(html);
   }
 });
-
-function isDirectory(path) {
-  return new Promise((resolve, reject) => {
-    fs.stat(path, (err, stats) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(stats.isDirectory());
-      }
-    });
-  });
-}
 
 function getContents(directoryPath) {
   return new Promise((resolve, reject) => {
@@ -104,4 +93,112 @@ function getContents(directoryPath) {
       }
     });
   });
+}
+
+function getIcon(fileName) {
+  fileName = fileName.toLowerCase();
+
+  if (
+    fileName.endsWith(".txt") ||
+    fileName.endsWith(".asc") ||
+    fileName.endsWith(".log")
+  ) {
+    return "text.svg"
+  } else if (
+    fileName.endsWith(".docx") ||
+    fileName.endsWith(".doc") ||
+    fileName.endsWith(".wps") ||
+    fileName.endsWith(".pdf") ||
+    fileName.endsWith(".pages") ||
+    fileName.endsWith(".rtf") ||
+    fileName.endsWith(".md")
+  ) {
+    return "textmedia.svg"
+  } else if (
+    fileName.endsWith(".js") ||
+    fileName.endsWith(".ts") ||
+    fileName.endsWith(".py") ||
+    fileName.endsWith(".html") ||
+    fileName.endsWith(".css") ||
+    fileName.endsWith(".ipynb")
+  ) {
+    return "code.svg"
+  } else if (
+    fileName.endsWith(".tar") ||
+    fileName.endsWith(".gz") ||
+    fileName.endsWith(".bz2") ||
+    fileName.endsWith(".zip") ||
+    fileName.endsWith(".rar")
+  ) {
+    return "archive.svg"
+  } else if (
+    fileName.endsWith(".epub") ||
+    fileName.endsWith(".azw3") ||
+    fileName.endsWith(".cbz")
+  ) {
+    return "book.svg"
+  } else if (
+    fileName.endsWith(".eml") ||
+    fileName.endsWith(".msg")
+  ) {
+    return "email.svg"
+  } else if (
+    fileName.endsWith(".mp4") ||
+    fileName.endsWith(".m4v") ||
+    fileName.endsWith(".m4p") ||
+    fileName.endsWith(".mkv") ||
+    fileName.endsWith(".mpv") ||
+    fileName.endsWith(".mp2") ||
+    fileName.endsWith(".mpg") ||
+    fileName.endsWith(".mpeg") ||
+    fileName.endsWith(".mov") ||
+    fileName.endsWith(".qt") ||
+    fileName.endsWith(".avi") ||
+    fileName.endsWith(".wmv") ||
+    fileName.endsWith(".flv") ||
+    fileName.endsWith(".swf") ||
+    fileName.endsWith(".avchd") ||
+    fileName.endsWith(".3gp") ||
+    fileName.endsWith(".mpe")
+  ) {
+    return "video.svg"
+  } else if (
+    fileName.endsWith(".srt") ||
+    fileName.endsWith(".890") ||
+    fileName.endsWith(".cip") ||
+    fileName.endsWith(".pac") ||
+    fileName.endsWith(".scc") ||
+    fileName.endsWith(".sub") ||
+    fileName.endsWith(".xml") ||
+    fileName.endsWith(".vtt") ||
+    fileName.endsWith(".edl") ||
+    fileName.endsWith(".stl")
+  ) {
+    return "subtitles.svg"
+  } else if (
+    fileName.endsWith(".ogg") ||
+    fileName.endsWith(".asf") ||
+    fileName.endsWith(".aiff") ||
+    fileName.endsWith(".flac") ||
+    fileName.endsWith(".alac") ||
+    fileName.endsWith(".mid") ||
+    fileName.endsWith(".midi") ||
+    fileName.endsWith(".aac") ||
+    fileName.endsWith(".acc") ||
+    fileName.endsWith(".mp3") ||
+    fileName.endsWith(".ac3") ||
+    fileName.endsWith(".mp2") ||
+    fileName.endsWith(".vqf") ||
+    fileName.endsWith(".qt") ||
+    fileName.endsWith(".waf") ||
+    fileName.endsWith(".wav") ||
+    fileName.endsWith(".ra") ||
+    fileName.endsWith(".pcm") ||
+    fileName.endsWith(".wma")
+  ) {
+    return "sound.svg"
+  } else {
+    console.log(color.yellow("File format not recognized:"), fileName.split(".")[1])
+    return "file.svg"
+  }
 }
